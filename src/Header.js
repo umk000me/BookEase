6a04e1c5b50fbde6f324f670f8cc49479c96bbf3
Umesh Mahendra Khairnar <130311083+umk000me@users.noreply.github.com>
2024-08-03 11:32:22 +0530

    xyz

--- src/Header.js
+++ src/Header.js
@@ -1,99 +1,82 @@
 import React from 'react'
 import tech from './techtitans.png'
-
 import booke from './booke.png'
 import { Link } from 'react-router-dom'
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-
 import Modal from 'react-bootstrap/Modal';
-import Demoform from './Demoform'
+import Demoform from './Demoform';
+import './App.css';  
+
 function Header() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
   return (
     <div className='navi'>
-      <div className='container_fluid'>
+      <div className='container-fluid'>
         <div className='row'>
-          <div className=' col-3  '> <img src={booke} alt="" width={300} height={43} /></div>
-          <div className='col-3'>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <a class="nav-link" href="#"> <button class="btn " type="submit"><i class="fa-solid fa-magnifying-glass"> </i></button></a>
-            </form>
-
+          <div className='col-6 col-md-3'>
+            <img src={booke} alt="Logo" className='img-fluid' />
           </div>
-
-
-
-          <div className='log col-3'>
-            <i class="fa-solid fa-heart"></i>
-            <i class="fa-solid fa-cart-shopping"></i> </div>
-          <div className='col-3' >
-            <>
-              <Button className='btnn' onClick={handleShow}>
-                Sign in<i class="fa-solid fa-user"></i>
-              </Button>
-<div>
-              <Modal  show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title className='hd'>Sign in</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                 
-                  <Demoform className='login'/>
-                </Modal.Body>
-                
-              </Modal>
-              </div>
-            </>
-
+          <div className='col-12 col-md-6'>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn" type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+          </div>
+          <div className='col-6 col-md-3 text-end'>
+            <i className="fa-solid fa-heart me-3"></i>
+            <i className="fa-solid fa-cart-shopping me-3"></i>
+            <Button className='btnn' onClick={handleShow}>
+              Sign in <i className="fa-solid fa-user"></i>
+            </Button>
           </div>
         </div>
-
       </div>
 
-      <nav class="navbar shadow-lg navbar-expand-lg bg-body-dark  ">
-        <div class="container-fluid">
-
-
-          <div class="collapse navbar-collapse d-flex justify-content-center " id="navbarSupportedContent">
-            <div className='cdn'>
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-                <li class="nav-item ">
-                  <a className="nav-link"   ><Link to='/' className=' abc'>Home</Link></a>
-                </li>
-                <li className="nav-item">
-                  <a class="nav-link" ><Link to='/arrivel' className=' abc'>Arrival</Link></a>
-                </li>
-
-                <li className="nav-item">
-                  <a class="nav-link" > <Link to='/feature' className=' abc'> Featured</Link> </a>
-                </li>
-
-
-               
-                
-                <li class="nav-item ">
-                  <a className="nav-link"   ><Link to='/aboutus' className=' abc'>About</Link></a>
-                </li>
-                
-                <li class="nav-item ">
-                  <a className="nav-link"   ><Link to='/team' className=' abc'>Team</Link></a>
-                </li>
-                <li className="nav-item">
-                  <a class="nav-link" ><Link to='/info' className=' abc'>Customer Information</Link></a>
-                </li>
-              </ul>
-            </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-lg mt-3">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item">
+                <Link to='/' className='nav-link'>Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/arrivel' className='nav-link'>Arrival</Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/feature' className='nav-link'>Featured</Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/aboutus' className='nav-link'>About</Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/team' className='nav-link'>Team</Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/info' className='nav-link'>Customer Information</Link>
+              </li>
+            </ul>
           </div>
         </div>
-      </nav></div>
+      </nav>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className='hd'>Sign in</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Demoform className='login' />
+        </Modal.Body>
+      </Modal>
+    </div>
   )
 }
 
-export default Header
-
+export default Header;
--- src/Home.js
+++ src/Home.js
@@ -1,301 +1,76 @@
-import React from 'react'
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import book1 from './book_22.jpg'
-import book2 from './book_23.jpg'
-import book3 from './book_24.jpg'
-import book4 from './book_25.jpg'
-import book5 from './book_5.jpg'
-import book6 from './book_6.jpg'
-import book7 from './book_20.jpg'
-import book8 from './book_19.jpg'
-import book9 from './book_9.jpg'
-import book10 from './book_11.jpg'
-import book11 from './book_16.jpg'
-import book12 from './book_17.jpg'
-
+import book1 from './book_22.jpg';
+import book2 from './book_23.jpg';
+import book3 from './book_24.jpg';
+import book4 from './book_25.jpg';
+import book5 from './book_5.jpg';
+import book6 from './book_6.jpg';
+import book7 from './book_20.jpg';
+import book8 from './book_19.jpg';
+import book9 from './book_9.jpg';
+import book10 from './book_11.jpg';
+import book11 from './book_16.jpg';
+import book12 from './book_17.jpg';
 import { Link } from 'react-router-dom';
+
 function Home() {
+  const books = [
+    { src: book1, author: "ETHAN MOLLICK", title: "Living And Working with AI", price: 350, originalPrice: 500 },
+    { src: book2, author: "RACHEL LINCH", title: "SILENT BONES", price: 440, originalPrice: 600 },
+    { src: book3, author: "ROBERT JORDAN", title: "THE EYE OF THE WORLD", price: 650, originalPrice: 700 },
+    { src: book4, author: "UNKNOWN", title: "ARTIFICIAL INTELLIGENCE AND MARKET ABUSE LEGISLATION", price: 880, originalPrice: 1000 },
+    { src: book5, author: "LUCKY CREHAN", title: "CLEVER LANDS", price: 270, originalPrice: 400 },
+    { src: book6, author: "UNKNOWN", title: "DICK FRANCIS SHATTERED", price: 195, originalPrice: 250 },
+    { src: book7, author: "ROBERT GREENE", title: "THE 48 LAWS OF POWER", price: 965, originalPrice: 1000 },
+    { src: book8, author: "JAY RATAN & P.LAL", title: "A NOVEL OF PEACENT INDIA", price: 350, originalPrice: 500 },
+    { src: book9, author: "PETER CAWDRON", title: "FREE FALL", price: 600, originalPrice: 665 },
+    { src: book10, author: "ETHAN MOLLICK", title: "BORING GIRLS THE NOVEL SARA TAYLOR", price: 150, originalPrice: 185 },
+    { src: book11, author: "RAKSHANDA JALIL", title: "THE LOHARU LEGACY", price: 250, originalPrice: 290 },
+    { src: book12, author: "SYEDA IMAN", title: "LIFE LIKE WINE", price: 350, originalPrice: 500 }
+  ];
+
   return (
-    <div>
-      
-        <div className='container-js'>
-            <div className='row'>
-            <div className=' mt-5 col-3'>
-           <Card style={{ width: '16rem' }}>
-  <div className='icons'>
-   </div>
-<div className='card'>
-     <Card.Img className=' cd p-3 bg-body-tertiary book-img' variant="top" src={book1} width={50} height={250} />
-      <Card.Body >
-        <Card.Title>By ETHAN MOLLICK</Card.Title>
-        <Card.Text><p className='pr'>Living And Working with AI</p>
-        <div className='rate'><i class="fa-solid fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i> <i class="fa-regular fa-star text-success"></i></div>
-       <div className='price'> Rs.350   <strike>Rs.500</strike> </div>
-        <div className='icons'>
-  <i class="fa-solid fa-magnifying-glass"></i>
-  <i class="fa-regular fa-heart"></i>
-  <i class="fa-regular fa-eye"></i>
-</div>
-        </Card.Text>
-        <Button className='button3'><Link className='link' to='/cart'>Buy now</Link></Button>
-      </Card.Body>
+    <div className='container-js'>
+      <div className='row'>
+        {books.map((book, index) => (
+          <div className='mt-5 col-3' key={index}>
+            <Card style={{ width: '95%' }}>
+              <div className='icons'></div>
+              <div className='card'>
+                <Card.Img className='cd p-3 bg-body-tertiary book-img' variant="top" src={book.src} width={50} height={250} />
+                <Card.Body>
+                  <Card.Title>By {book.author}</Card.Title>
+                  <Card.Text>
+                    <p className='pr'>{book.title}</p>
+                    <div className='rate'>
+                      <i className="fa-solid fa-star text-success"></i>
+                      <i className="fa-solid fa-star text-success"></i>
+                      <i className="fa-solid fa-star text-success"></i>
+                      <i className="fa-solid fa-star text-success"></i>
+                      <i className="fa-regular fa-star text-success"></i>
+                    </div>
+                    <div className='price'>
+                      Rs.{book.price} <strike>Rs.{book.originalPrice}</strike>
+                    </div>
+                    <div className='icons'>
+                      <i className="fa-solid fa-magnifying-glass"></i>
+                      <i className="fa-regular fa-heart"></i>
+                      <i className="fa-regular fa-eye"></i>
+                    </div>
+                  </Card.Text>
+                  <Button className='button3'>
+                    <Link className='link' to='/cart'>Buy now</Link>
+                  </Button>
+                </Card.Body>
+              </div>
+            </Card>
+          </div>
+        ))}
       </div>
-    </Card>
     </div>
-
-    <div className=' mt-5 col-3'>
-           <Card style={{ width: '16rem' }}>
-  <div className='icons'>
-   </div>
-<div className='card'>
-     <Card.Img className=' cd p-3 bg-body-tertiary book-img' variant="top" src={book2} width={50} height={250} />
-      <Card.Body >
-        <Card.Title>By RACHEL LINCH</Card.Title>
-        <Card.Text><p className='pr'>SILENT BONES</p>
-        <div className='rate'><i class="fa-solid fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i> <i class="fa-regular fa-star text-success"></i></div>
-       <div className='price'> Rs.440   <strike>Rs.600</strike> </div>
-        <div className='icons'>
-  <i class="fa-solid fa-magnifying-glass"></i>
-  <i class="fa-regular fa-heart"></i>
-  <i class="fa-regular fa-eye"></i>
-</div>
-        </Card.Text>
-        <Button className='button3'><Link className='link' to='./cart'>Buy now</Link></Button>
-      </Card.Body>
-      </div>
-    </Card>
-    </div>
-
-    <div className=' mt-5 col-3'>
-           <Card style={{ width: '16rem' }}>
-  <div className='icons'>
-   </div>
-<div className='card'>
-     <Card.Img className=' cd p-3 bg-body-tertiary book-img' variant="top" src={book3} width={50} height={250} />
-      <Card.Body >
-        <Card.Title>By ROBERT JORDAN</Card.Title>
-        <Card.Text><p className='pr'>THE EYE OF THE WORLD</p>
-        <div className='rate'><i class="fa-solid fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i> <i class="fa-regular fa-star text-success"></i></div>
-       <div className='price'> Rs.650   <strike>Rs.700</strike> </div>
-        <div className='icons'>
-  <i class="fa-solid fa-magnifying-glass"></i>
-  <i class="fa-regular fa-heart"></i>
-  <i class="fa-regular fa-eye"></i>
-</div>
-        </Card.Text>
-        <Button className='button3' ><Link className='link' to='/cart'>Buy now</Link></Button>      </Card.Body>
-      </div>
-    </Card>
-    </div>
-
-    <div className=' mt-5 col-3'>
-           <Card style={{ width: '16rem' }}>
-  <div className='icons'>
-   </div>
-<div className='card'>
-     <Card.Img className=' cd p-3 bg-body-tertiary book-img' variant="top" src={book4} width={50} height={250} />
-      <Card.Body >
-        <Card.Title>By UNKNOWN</Card.Title>
-        <Card.Text><p className='pr'>ARTIFICIAL INTELLIGENCE AND MARKET ABUSE LEGISLATION</p>
-        <div className='rate'><i class="fa-solid fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i> <i class="fa-regular fa-star text-success"></i></div>
-       <div className='price'> Rs.880   <strike>Rs.1000</strike> </div>
-        <div className='icons'>
-  <i class="fa-solid fa-magnifying-glass"></i>
-  <i class="fa-regular fa-heart"></i>
-  <i class="fa-regular fa-eye"></i>
-</div>
-        </Card.Text>
-        <Button className='button3'><Link className='link' to='/cart'>Buy now</Link></Button>      </Card.Body>
-      </div>
-    </Card>
-    </div>
-
-    <div className=' mt-5 col-3'>
-           <Card style={{ width: '16rem' }}>
-  <div className='icons'>
-   </div>
-<div className='card'>
-     <Card.Img className=' cd p-3 bg-body-tertiary book-img' variant="top" src={book5} width={50} height={250} />
-      <Card.Body >
-        <Card.Title>By LUCKY CREHAN</Card.Title>
-        <Card.Text><p className='pr'>CLEVER LANDS</p>
-        <div className='rate'><i class="fa-solid fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i> <i class="fa-regular fa-star text-success"></i></div>
-       <div className='price'> Rs.270   <strike>Rs.400</strike> </div>
-        <div className='icons'>
-  <i class="fa-solid fa-magnifying-glass"></i>
-  <i class="fa-regular fa-heart"></i>
-  <i class="fa-regular fa-eye"></i>
-</div>
-        </Card.Text>
-        <Button className='button3'><Link className='link' to='/cart'>Buy now</Link></Button>      </Card.Body>
-      </div>
-    </Card>
-    </div>
-
-    <div className=' mt-5 col-3'>
-           <Card style={{ width: '16rem' }}>
-  <div className='icons'>
-   </div>
-<div className='card'>
-     <Card.Img className=' cd p-3 bg-body-tertiary book-img' variant="top" src={book6} width={50} height={250} />
-      <Card.Body >
-        <Card.Title>By UNKNOWN</Card.Title>
-        <Card.Text><p className='pr'>DICK FRANCIS SHATTERED</p>
-        <div className='rate'><i class="fa-solid fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i> <i class="fa-regular fa-star text-success"></i></div>
-       <div className='price'> Rs.195   <strike>Rs.250</strike> </div>
-        <div className='icons'>
-  <i class="fa-solid fa-magnifying-glass"></i>
-  <i class="fa-regular fa-heart"></i>
-  <i class="fa-regular fa-eye"></i>
-</div>
-        </Card.Text>
-        <Button className='button3'><Link className='link' to='/cart'>Buy now</Link></Button>      </Card.Body>
-      </div>
-    </Card>
-    </div>
-
-
-    <div className=' mt-5 col-3'>
-           <Card style={{ width: '16rem' }}>
-  <div className='icons'>
-   </div>
-<div className='card'>
-     <Card.Img className=' cd p-3 bg-body-tertiary book-img' variant="top" src={book7} width={50} height={250} />
-      <Card.Body >
-        <Card.Title>By ROBERT GREENE</Card.Title>
-        <Card.Text><p className='pr'>THE 48 LAWS OF POWER</p>
-        <div className='rate'><i class="fa-solid fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i> <i class="fa-regular fa-star text-success"></i></div>
-       <div className='price'> Rs.965   <strike>Rs.1000</strike> </div>
-        <div className='icons'>
-  <i class="fa-solid fa-magnifying-glass"></i>
-  <i class="fa-regular fa-heart"></i>
-  <i class="fa-regular fa-eye"></i>
-</div>
-        </Card.Text>
-        <Button className='button3'><Link className='link' to='/cart'>Buy now</Link></Button>      </Card.Body>
-      </div>
-    </Card>
-    </div>
-
-
-    <div className=' mt-5 col-3'>
-           <Card style={{ width: '16rem' }}>
-  <div className='icons'>
-   </div>
-<div className='card'>
-     <Card.Img className=' cd p-3 bg-body-tertiary book-img' variant="top" src={book8} width={50} height={250} />
-      <Card.Body >
-        <Card.Title>By JAY RATAN & P.LAL</Card.Title>
-        <Card.Text><p className='pr'>A NOVEL OF PEACENT INDIA</p>
-        <div className='rate'><i class="fa-solid fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i> <i class="fa-regular fa-star text-success"></i></div>
-       <div className='price'> Rs.350   <strike>Rs.500</strike> </div>
-        <div className='icons'>
-  <i class="fa-solid fa-magnifying-glass"></i>
-  <i class="fa-regular fa-heart"></i>
-  <i class="fa-regular fa-eye"></i>
-</div>
-        </Card.Text>
-        <Button className='button3'><Link className='link' to='/cart'>Buy now</Link></Button>      </Card.Body>
-      </div>
-    </Card>
-    </div>
-
-    <div className=' mt-5 col-3'>
-           <Card style={{ width: '16rem' }}>
-  <div className='icons'>
-   </div>
-<div className='card'>
-     <Card.Img className=' cd p-3 bg-body-tertiary book-img' variant="top" src={book9} width={50} height={250} />
-      <Card.Body >
-        <Card.Title>By PETER CAWDRON</Card.Title>
-        <Card.Text><p className='pr'>FREE FALL</p>
-        <div className='rate'><i class="fa-solid fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i> <i class="fa-regular fa-star text-success"></i></div>
-       <div className='price'> Rs.600   <strike>Rs.665</strike> </div>
-        <div className='icons'>
-  <i class="fa-solid fa-magnifying-glass"></i>
-  <i class="fa-regular fa-heart"></i>
-  <i class="fa-regular fa-eye"></i>
-</div>
-        </Card.Text>
-        <Button className='button3'><Link className='link' to='/cart'>Buy now</Link></Button>      </Card.Body>
-      </div>
-    </Card>
-    </div>
-
-
-    <div className=' mt-5 col-3'>
-           <Card style={{ width: '16rem' }}>
-  <div className='icons'>
-   </div>
-<div className='card'>
-     <Card.Img className=' cd p-3 bg-body-tertiary book-img' variant="top" src={book10} width={50} height={250} />
-      <Card.Body >
-        <Card.Title>By ETHAN MOLLICK</Card.Title>
-        <Card.Text><p className='pr'>BORING GIRLS THE NOVEL SARA TAYLOR</p>
-        <div className='rate'><i class="fa-solid fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i> <i class="fa-regular fa-star text-success"></i></div>
-       <div className='price'> Rs.150   <strike>Rs.185</strike> </div>
-        <div className='icons'>
-  <i class="fa-solid fa-magnifying-glass"></i>
-  <i class="fa-regular fa-heart"></i>
-  <i class="fa-regular fa-eye"></i>
-</div>
-        </Card.Text>
-        <Button className='button3'><Link className='link' to='/cart'>Buy now</Link></Button>      </Card.Body>
-      </div>
-    </Card>
-    </div>
-
-    <div className=' mt-5 col-3'>
-           <Card style={{ width: '16rem' }}>
-  <div className='icons'>
-   </div>
-<div className='card'>
-     <Card.Img className=' cd p-3 bg-body-tertiary book-img' variant="top" src={book11} width={50} height={250} />
-      <Card.Body >
-        <Card.Title>By RAKSHANDA JALIL</Card.Title>
-        <Card.Text><p className='pr'>THE LOHARU LEGACY</p>
-        <div className='rate'><i class="fa-solid fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i> <i class="fa-regular fa-star text-success"></i></div>
-       <div className='price'> Rs.250   <strike>Rs.290</strike> </div>
-        <div className='icons'>
-  <i class="fa-solid fa-magnifying-glass"></i>
-  <i class="fa-regular fa-heart"></i>
-  <i class="fa-regular fa-eye"></i>
-</div>
-        </Card.Text>
-        <Button className='button3'><Link className='link' to='/cart'>Buy now</Link></Button>      </Card.Body>
-      </div>
-    </Card>
-    </div>
-
-    <div className=' mt-5 col-3'>
-           <Card style={{ width: '16rem' }}>
-  <div className='icons'>
-   </div>
-<div className='card'>
-     <Card.Img className=' cd p-3 bg-body-tertiary book-img' variant="top" src={book12} width={50} height={250} />
-      <Card.Body >
-        <Card.Title>By SYEDA IMAN</Card.Title>
-        <Card.Text><p className='pr'>LIFE LIKE WINE</p>
-        <div className='rate'><i class="fa-solid fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i><i class="fa-solid  fa-star text-success"></i> <i class="fa-regular fa-star text-success"></i></div>
-       <div className='price'> Rs.350   <strike>Rs.500</strike> </div>
-        <div className='icons'>
-  <i class="fa-solid fa-magnifying-glass"></i>
-  <i class="fa-regular fa-heart"></i>
-  <i class="fa-regular fa-eye"></i>
-</div>
-        </Card.Text>
-        <Button className='button3'><Link className='link' to='/cart'>Buy now</Link></Button>      </Card.Body>
-      </div>
-    </Card>
-    </div>
-
-    
-  
-            </div>
-        </div>
-    </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
